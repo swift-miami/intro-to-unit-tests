9cb0fbf654e29cb0d656dbcac1d9804525688c36
Tommy <Tomjen1145@gmail.com>
2019-04-03 12:39:15 -0400

    Email List: Created table view, search bar, labels and text field.

---
+++ EmailListV5/TableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewCell.swift
+//  EmailListV5
+//
+//  Created by user on 4/2/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- EmailListV5/ViewController.swift
+++ EmailListV5/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct cellData {
+    let label : String
+    let text : String
+    
+}
 
+class TableViewController: UITableViewController, UISearchBarDelegate {
+
+    
+    var arrayOfCellData = [cellData(label: "Hello", text: "Where they at tho?"),
+                           cellData(label: "Hello", text: "Where they at tho?"),
+                           cellData(label: "Hello", text: "Where they at tho?")]
+    
+    lazy var searchBar: UISearchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 200, height: 20))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpTableView()
+        
+        self.navigationItem.titleView = searchBar
+        searchBar.showsCancelButton = true
+        searchBar.delegate = self
     }
 
 
+    
+    func setUpTableView() {
+        
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfCellData.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = arrayOfCellData[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        cell.label.text = item.label
+        cell.textField.text = item.text
+        return cell
+    }
 }
 
